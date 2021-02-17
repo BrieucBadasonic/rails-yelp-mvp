3968db33e902343dfa834e8cca9f58127ba2f067
Brieuc Labiouse <brieuclabiouse@gmail.com>
2021-02-17 14:22:16 +0100

    new route is working with good redirection

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,6 +6,25 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :phone_number, :address, :category)
+  end
 end
 
 #            Prefix Verb URI Pattern                                       Controller#Action
