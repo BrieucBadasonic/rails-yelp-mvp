37b46c72f2b845f1bd8c759834317ef985a037e0
Brieuc Labiouse <brieuclabiouse@gmail.com>
2021-02-17 13:07:57 +0100

    index route is working

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,17 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+
+  end
 end
+
+#            Prefix Verb URI Pattern                                       Controller#Action
+#    restaurant_reviews POST /restaurants/:restaurant_id/reviews(.:format)     reviews#create
+# new_restaurant_review GET  /restaurants/:restaurant_id/reviews/new(.:format) reviews#new
+#           restaurants GET  /restaurants(.:format)                            restaurants#index
+#                       POST /restaurants(.:format)                            restaurants#create
+#        new_restaurant GET  /restaurants/new(.:format)                        restaurants#new
+#            restaurant GET  /restaurants/:id(.:format)                        restaurants#show
