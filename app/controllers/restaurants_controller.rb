fc8a14ee9aee492752cfe834232ceaeca0a35333
Brieuc Labiouse <brieuclabiouse@gmail.com>
2021-02-17 16:49:13 +0100

    little bit more styling

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -26,11 +26,3 @@ class RestaurantsController < ApplicationController
     params.require(:restaurant).permit(:name, :phone_number, :address, :category)
   end
 end
-
-#            Prefix Verb URI Pattern                                       Controller#Action
-#    restaurant_reviews POST /restaurants/:restaurant_id/reviews(.:format)     reviews#create
-# new_restaurant_review GET  /restaurants/:restaurant_id/reviews/new(.:format) reviews#new
-#           restaurants GET  /restaurants(.:format)                            restaurants#index
-#                       POST /restaurants(.:format)                            restaurants#create
-#        new_restaurant GET  /restaurants/new(.:format)                        restaurants#new
-#            restaurant GET  /restaurants/:id(.:format)                        restaurants#show
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
+  CAT = ["Chinese", "Italian", "Japanese", "French", "Belgian"]
   has_many :reviews, dependent: :destroy
   validates :name, :address, presence: true
   validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
